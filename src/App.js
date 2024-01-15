0119e3c1d728ccd32accc3e3ea9832318a1c0837
AlexKasema <lexxmulwa@gmail.com>
2024-01-15 14:46:13 +0300

    Implemented winning condition

--- src/App.js
+++ src/App.js
@@ -8,22 +8,39 @@ import { nanoid } from "nanoid"
 export default function App(){
 
     const [dice, setDice] = React.useState(allNewDice())
+    const [tenzie, setTenzies] = React.useState(false)
+
+    React.useEffect(() => {
+        const allHeld = dice.every(die => die.isHeld)
+        const firstValue = dice[0].value
+        const allSameValue = dice.every(die => die.value === firstValue)
+
+        if (allHeld && allSameValue){
+            setTenzies(true)
+            console.log("You won!")
+        }
+    }, [dice])
+
+    function generateNewDie(){
+        return {
+            value: Math.ceil(Math.random() * 6),
+            isHeld: false,
+            id: nanoid()
+        }
+    }
 
     function allNewDice(){
         const newDice = []
         for (let i = 0; i < 10; i++){
-            newDice.push({
-                value: Math.ceil(Math.random() * 6),
-                isHeld: false,
-                id: nanoid()
-            })
+            newDice.push(generateNewDie())
         }
-
         return newDice
     }
 
     function rollDice(){
-        setDice(allNewDice)
+        setDice(oldDice => oldDice.map(die => {
+            return die.isHeld ? die : generateNewDie()
+        }))
     }
 
     function holdDice(id){
@@ -41,6 +58,8 @@ export default function App(){
 
     return (
         <main>
+            <h1 className="title">Tenzies</h1>
+            <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
             <div className="dice--container">
                 {diceElements}
             </div>
